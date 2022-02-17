6471a4e91c5df42cdd3c93b4ac051a11cee5f883
Guilherme <56496944+glhrmarques@users.noreply.github.com>
2022-02-17 01:16:58 -0300

    Create practice.js

---
+++ array/practice.js
@@ -0,0 +1,23 @@
+// It's a common practice to declare arrays with const 
+
+//Acessing the element by referring its index number
+const cars = ['Slow', 'Fast']
+let car = cars[1]
+
+//Objects use named indexes to acess its 'members', on the other hand, arrays use numbers.
+const nameGenerator = [firstName= 'Guilherme', secondName='Andressa'];
+const carGenerator = ['Carro1', 'Carro2'];
+
+let name = firstName;
+let carPrint = carGenerator[1]
+
+//Array Properties
+const fruits = ['Passion fruit', 'Strawberry', 'Grape']
+let length = fruits.length;
+
+//looping the array
+
+
+//push() adds a new element to the array.]
+const testing = ['Passion fruit', 'Strawberry', 'Grape']
+testing.push('Lemon');
