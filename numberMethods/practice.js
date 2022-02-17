06e5e7a280ae7addabd1c40d54286c3c27737690
Guilherme <56496944+glhrmarques@users.noreply.github.com>
2022-02-17 01:19:04 -0300

    Create practice.js

---
+++ numberMethods/practice.js
@@ -0,0 +1,15 @@
+// toString() returns a number as a string
+var x = 3;
+x.toString();
+(130).toString();
+(3 + 1).toString();
+
+
+//toFixed() returns a specified number of decimals
+var y = 3.123.toFixed(2)
+
+//Number() converts variables to numbers
+var number = '123'
+var converter = Number(number)
+
+//
