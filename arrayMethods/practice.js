cbe0f0e9da544c8d7892d181d68f655a751a5501
Guilherme <56496944+glhrmarques@users.noreply.github.com>
2022-02-17 01:17:24 -0300

    Create practice.js

---
+++ arrayMethods/practice.js
@@ -0,0 +1,11 @@
+// pop() removes the last element in the array
+const fruits1 = ['Avocado', 'Pineaple', 'Watermelon'];
+let x = fruits1.pop()
+
+// shift() removes the first element in the array
+const fruits2 = ['Avocado', 'Pineaple', 'Watermelon'];
+let y = fruits2.shift()
+
+//unshift() adds the an element in the beginning of an array
+const fruits3 = ['Avocado', 'Pineaple', 'Watermelon'];
+let z = fruits3.unshift('Something')
