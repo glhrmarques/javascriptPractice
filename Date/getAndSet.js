57caaee07ac9a400c60d2d09d74ee3e9ffef02a6
Guilherme <56496944+glhrmarques@users.noreply.github.com>
2022-03-02 22:59:14 -0300

    Create getAndSet.js

---
+++ Date/getAndSet.js
@@ -0,0 +1,31 @@
+//to know the current date and time
+const date = new Date();
+
+
+/*
+ There are 4 ways to create a new date object 
+
+new Date()
+new Date(year, month, day, hours, minutes, seconds, milliseconds)
+new Date(milliseconds)
+new Date(date string)
+ */
+
+
+// The method toDateString converts a date to a more readable format
+const date02 = new Date();
+date02.toDateString();
+
+/*
+There are 2 types of methods GET and SET
+GET - returns the current state
+SET - return the state set
+
+GET
+getDate(), getMonth(), getDate(), getHours(), getMinutes(), getSeconds(), getMiliseconds(), getTime(), getDay()
+
+SET
+setDate(), setFullYear(), setHours(), setMiliseconds(), setMinutes(), setMonth(), setSeconds(), setTime()
+
+ 
+ */
