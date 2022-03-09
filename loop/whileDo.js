c933187ae6937156255c443f4114fda587bdba1c
Guilherme <56496944+glhrmarques@users.noreply.github.com>
2022-03-09 20:40:32 -0300

    Create whileDo.js

---
+++ loop/whileDo.js
@@ -0,0 +1,21 @@
+// 1 statement, is the condition
+// 2 statement, code to be executed
+
+
+let str =  1;
+
+while ( str <= 6) {
+    console.log( str + 'position');
+    str++
+}
+
+// 1 statement, code to be executed
+// 2 statement, is the condition
+
+let str2 =  1;
+
+do {
+    console.log(str2 + 'position');
+    str2++
+} while ( str2 <= 10)
+
